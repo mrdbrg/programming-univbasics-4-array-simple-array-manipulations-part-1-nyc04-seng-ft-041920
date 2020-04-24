938885864cfbfe36daeb862da5b2ce5e38e61e9d
Marlon Braga <rdg.brg@gmail.com>
2020-04-24 01:57:53 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, str)
+  array.push(str)
+end
+
+def using_unshift(array, str)
+  array.unshift(str)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array) 
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
